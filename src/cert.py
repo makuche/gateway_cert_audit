d0ae15d6d8dc0f6f7205e079e312f0ab3df9bff3
Manuel Kuchelmeister <manuel.kuchelmeister@web.de>
2024-02-07 15:20:13 +0100

    set correct repo path, fix typos

--- src/cert.py
+++ src/cert.py
@@ -4,13 +4,25 @@ from cryptography.hazmat.primitives.serialization import Encoding
 class X509Certificate():
     def __init__(self, cert):
         self.serial_nr = str(cert.serial_number)
-        self.fingerprint = str(self.fingerprint)
-        self.issuer = str(self.issuer)
-        self.start = str(self.start)
-        self.end = str(self.end)
-        self.public_bytes = str(self.public_bytes)
-        self.subject = str(self.subject)
+        self.fingerprint = str(cert.fingerprint(hashes.SHA512().hex()))
+        self.issuer = str(cert.issuer.rfc4514_string())
+        self.start = str(cert.not_valid_before)
+        self.end = str(cert.not_valid_after)
+        self.public_bytes = str(cert.public_bytes(Encoding.PEM))
+        self.subject = str(cert.subject.rfc4514_string())
 
+    def to_dict(self):
+        return vars(self)
+
+    def is_valid(self):
+        # TODO: Return start.end > current_datetime
+        pass
+
+    def expires_soon(self):
+        # TODO: Returns true if either
+            # 1) is_valid is true, or
+            # 2) if (start.end - 3 months) < current_datetime
+        pass
 class Policy():
     pass
 
