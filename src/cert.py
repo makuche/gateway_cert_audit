665adb910a97be5521d26af38772deaf02d7767c
Manuel Kuchelmeister <manuel.kuchelmeister@web.de>
2024-02-09 14:28:14 +0100

    Fix certificate initialization

--- src/cert.py
+++ src/cert.py
@@ -7,9 +7,9 @@ class X509Certificate():
     def __init__(self, cert):
         self.serial_nr = str(hex(cert.serial_number))[2:]
         self.serial_nr_decimal = str(cert.serial_number)
-        self.fingerprint_SHA1 = str(cert.fingerprint(hashes.SHA1().hex()))
-        self.fingerprint_SHA256 = str(cert.fingerprint(hashes.SHA256().hex()))
-        self.fingerprint_SHA512 = str(cert.fingerprint(hashes.SHA512().hex()))
+        self.fingerprint_SHA1 = str(cert.fingerprint(hashes.SHA1()).hex())
+        self.fingerprint_SHA256 = str(cert.fingerprint(hashes.SHA256()).hex())
+        self.fingerprint_SHA512 = str(cert.fingerprint(hashes.SHA512()).hex())
         self.issuer = str(cert.issuer.rfc4514_string())
         self.start = str(cert.not_valid_before)
         self.end = str(cert.not_valid_after)
@@ -17,7 +17,7 @@ class X509Certificate():
         self.subject = str(cert.subject.rfc4514_string())
 
         self.expires_within_three_months = self.expiring_soon()
-        self.expires_next_month = self.expires_soon(time_delta=31)
+        self.expires_next_month = self.expiring_soon(time_delta=31)
         self.priv_key_on_hsm = None
 
     def to_dict(self):
